127b806d4c6daab08d231680138eadfbddca7703
marckes04 <maresp95@gmail.com>
2022-05-18 23:56:13 -0500

    new files were added

---
+++ SpyWord/Assets/Scripts/GameOverPopUp.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverPopUp : MonoBehaviour
+{
+    public GameObject gameoverpopup;
+    public GameObject continueAfterAdsButton;
+
+
+    void Start()
+    {
+        continueAfterAdsButton.GetComponent<Button>().interactable = false;
+        gameoverpopup.SetActive(false);
+
+        GameEvents.OnGameOver += ShowGameOverPopUp;
+    }
+
+    private void OnDisable()
+    {
+        GameEvents.OnGameOver -= ShowGameOverPopUp;
+    }
+
+    private void ShowGameOverPopUp()
+    {
+        gameoverpopup.SetActive(true);
+        continueAfterAdsButton.GetComponent<Button>().interactable = false;
+
+    }
+}
